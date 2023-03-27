b32bfa885ecfc3cfe098b0d1e8cda60883e5bacd
Vlad Lytvynets <vlad.lytvynets@icloud.com>
2023-03-27 15:56:26 +0300

    Initial Commit

---
+++ SearchMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchMovies
+//
+//  Created by Vlad Lytvynets on 27.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
